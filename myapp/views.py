8ddb6cbe28596bfe9b9337c58584d093b6cf2322
Brandon Oh <brandonoh2938@gmail.com>
2023-07-15 03:36:42 -0400

    added required import and require http methods

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
+@require_http_methods(['GET', 'POST'])
 def home(request):
     if request.method == 'GET':
         # Handle GET request
