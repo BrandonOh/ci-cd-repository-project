d46f43b324f842783ef8b8e6ed661834d1c10f9a
Brandon Oh <brandonoh2938@gmail.com>
2023-07-15 03:30:01 -0400

    added safe HTTP methods for routes/controllers

--- doguide/urls.py
+++ doguide/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import home
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', views.home),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'myapp/home.html')
+    if request.method == 'GET':
+        # Handle GET request
+        data = {'message': 'This is a GET request'}
+        return render(request, './myapp/home.html', data)
+    elif request.method == 'POST':
+        # Handle POST request
+        return HttpResponse('This is a POST request')
